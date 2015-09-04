84e0a10c8fc3119ed0525a9fff9ceeec77efe2a8
gennaro <gennaro.raiola@gmail.com>
2015-09-04 13:56:00 +0200

    Select index if we are in root

--- includes/config.php
+++ includes/config.php
@@ -1,12 +1,25 @@
 <html>
 
 <?php
+function echoSelectedRoot()
+{
+    $current_file_name = basename($_SERVER['REQUEST_URI'], ".php");
+    $site_root = basename($GLOBALS['SITE_ROOT'], ".php");
+    
+    if ($current_file_name == $site_root)
+        echo 'class="selected"'; 
+}
+?>
+
+<?php
 function echoSelectedClassIfRequestMatches($requestUri)
 {
     $current_file_name = basename($_SERVER['REQUEST_URI'], ".php");
 
+    $site_root = basename($GLOBALS['SITE_ROOT'], ".php");
+    
     if ($current_file_name == $requestUri)
-        echo 'class="selected"';
+        echo 'class="selected"'; 
 }
 ?>
 
--- includes/menu.php
+++ includes/menu.php
@@ -1,10 +1,15 @@
 <html>
 
+
+
 <div id="navbg">
 		<div id="nav">
 			<ul>	
-				
-			<li <?=echoSelectedClassIfRequestMatches("index")?>><a href="index.php">Home</a></li>
+
+			<li <?=echoSelectedClassIfRequestMatches("index")?>
+			<?=echoSelectedRoot()?>> <!-- Check if we are in the root, if yes, select index in the menu bar -->
+			<a href="index.php">Home</a></li>
+			
 			<li <?=echoSelectedClassIfRequestMatches("research")?>><a href="research.php">Research</a></li>
 			<li <?=echoSelectedClassIfRequestMatches("publications")?>><a href="publications.php">Publications</a></li>
 			<li <?=echoSelectedClassIfRequestMatches("videos")?>><a href="videos.php">Videos</a></li>
